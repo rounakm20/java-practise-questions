068003eea151dbbc9cd990874d1ade615439c800
Rounak Mishra <rounakm5886@gmail.com>
2026-03-16 00:29:33 +0530

    add reverse right triangle star pattern program in java

--- ReverseRightTrianglePattern.java
+++ ReverseRightTrianglePattern.java
@@ -7,17 +7,16 @@
 // ***
 // **
 // *
-
-
-public class App {
+public class ReverseRightTrianglePattern {
     public static void main(String[] args) {
         int n = 5;
-        for(int i =n; i>=1 ; i--){
-            for(int j = 1; j<=i ; j++){
-                System.out.print("*");
 
+        for (int i = n; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
             }
             System.out.println();
         }
     }
 }
+
