82b34fa0ff757381bd1fdee78e47267496ed763c
Rounak Mishra <rounakm5886@gmail.com>
2026-03-16 00:26:33 +0530

    Repeated Row Number Pattern implementation

--- RepeatedRowNumberPattern.java
+++ RepeatedRowNumberPattern.java
@@ -9,18 +9,17 @@
 // 4444
 // 55555
 
-
-
-public class App {
+public class RepeatedRowNumberPattern {
     public static void main(String[] args) {
         int n = 5;
-        for(int i =1; i<=n ; i++){
-            for(int j = 1; j <=i ; j++){
-           
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
                 System.out.print(i);
             }
-            
             System.out.println();
         }
     }
 }
+
+
